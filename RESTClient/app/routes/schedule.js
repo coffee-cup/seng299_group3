b29626c689b44ded335afe16f04a337ed1681433
Jake Runzer <jakerunzer@gmail.com>
2015-06-30 13:44:22 -0700

    basic rooms setup

--- RESTClient/app/routes/schedule.js
+++ RESTClient/app/routes/schedule.js
@@ -1,5 +1,38 @@
+var rooms = [
+  {
+    name: 'Room 1'
+  },
+  {
+    name: 'Room 2'
+  },
+  {
+    name: 'Room 3'
+  },
+  {
+    name: 'Room 4'
+  },
+  {
+    name: 'Room 5'
+  },
+  {
+    name: 'Room 6'
+  },
+  {
+    name: 'Room 7'
+  },
+  {
+    name: 'Room 8'
+  },
+  {
+    name: 'Room 9'
+  },
+  {
+    name: 'Room 10'
+  }
+];
 
 export default Ember.Route.extend({
   model: function() {
+    return rooms;
   }
 });
