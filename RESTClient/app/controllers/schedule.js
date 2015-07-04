497c609a6a980b4e08f5f33666d9dc12f1d27036
Jake Runzer <jakerunzer@gmail.com>
2015-07-04 16:51:07 -0700

    font change and schedule calendar update

--- RESTClient/app/controllers/schedule.js
+++ RESTClient/app/controllers/schedule.js
@@ -1,3 +1,44 @@
+var scope = {
+  this: null
+};
+
 export default Ember.Controller.extend({
-  
+
+  init : function(){
+    this._super();
+    scope.this = this;
+    Ember.run.scheduleOnce('afterRender', this, this.afterRenderEvent);
+  },
+
+  afterRenderEvent : function(){
+    // implement this hook in your own subclasses and run your jQuery logic there
+    $('#sch-calendar').datepicker({});
+
+    $('#sch-calendar').datepicker()
+      .on('changeDate', function(e) {
+        $("#my_hidden_input").val(
+            $("#sch-calendar").datepicker('getFormattedDate')
+
+         );
+         scope.this.send('dateChanged');
+      });
+
+    this.send('setDateToday');
+  },
+
+  actions: {
+    dateChanged: function() {
+      var d = new Date($('#my_hidden_input').val());
+      console.log(d);
+      // Call server here to refresh the model with a call to getAvailableRooms(d)
+    },
+
+    setDateToday: function() {
+      // for some reason have to make a new date this way,
+      // otherwise it does not select the day in the calendar
+      var d = new Date();
+      $('#sch-calendar').datepicker('setDate', new Date(d.getFullYear(), d.getMonth(), d.getDate()));
+    }
+  }
+
 });
