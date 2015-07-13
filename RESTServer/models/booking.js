406d5d7425559ee5c1443be7e34bbb24418fd5e0
Maxwell Borden <maxwellborden@gmail.com>
2015-07-13 02:22:40 -0700

    updating getavailability

--- RESTServer/models/booking.js
+++ RESTServer/models/booking.js
@@ -4,25 +4,29 @@ var Room = require('./room');
 var Equipment = require('./equipment');
 
 var BookingSchema = new Schema({
-    username: String,
-    numberOfPeople: Number,
-    date: Date,
-    canceledStatus: Boolean,
-    startTime: Number, //temporarily set to number
-    endTime: Number,    //temporarily set to number
-    room: [Room],
-    equipment: Array
+  username: String,
+  numberOfPeople: Number,
+  date: Date,
+  canceledStatus: Boolean,
+  startTime: Number, //temporarily set to number
+  endTime: Number,    //temporarily set to number
+  room: [Room],
+  equipment: Array
 });
 
-module.exports = mongoose.model('Booking', BookingSchema);
+var booking = mongoose.model('Booking', BookingSchema);
 
-module.exports.roomAvailability = function (date, roomid) {
-  Booking.find({'date':date, 'room.roomID':roomid}, function(err, bookings) {
+var roomAvailability = function (date, roomid) {
+  queryDate = new Date(date);
+  var bookList = [];
+  var async_done = false;
+  //{$and : [{'date': { $gte : queryDate}}, {'date' : { $lte : queryDate.getDate() +1 }}]}
+  booking.find({}, function(err, bookings) {
     if(err) {
-      res.send(err);
+      console.log(err);
     }
-    var bookingList = [];
-    if ([0,5,6].indexOf(req.query.date.getDay()) != -1){
+      console.log(bookings);
+    if ([0,5,6].indexOf(queryDate.getDay()) != -1){
       for(i = 14; i < 27; i++) {
         bookingList.push({
           "booked": false,
@@ -38,19 +42,30 @@ module.exports.roomAvailability = function (date, roomid) {
         });
       }
     }
-    bookings.sort({'startTime': 1}).toArray(function(err, bookings) {
-      if(err) {
-        res.send(err);
+    if(err) {
+      console.log(err);
+    }
+    for(var slot in bookingList){
+      for(var booking in bookings) {
+        if (slot.time >= booking.startTime && slot.time <= booking.endTime){
+          slot.booked = true;
+          break;
+        }
       }
-      for(var slot in bookingList){
-          for(var booking in bookings) {
-              if (slot.time >= booking.startTime && slot.time <= booking.endTime){
-              slot.booked = true;
-              break;
-              }
-          }
-      }
-      return bookingList;
-    });
+    }
+
+    async_done = true;
+    console.log('done');
   });
+
+  while(!async_done) {
+    continue;
+  }
+
+  return bookingList;
 }
+module.exports = {
+  booking: booking,
+  roomAvailability: roomAvailability
+}
+
