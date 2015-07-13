85499a2c5008406a35d584367783b97eb69ed253
Spencer <mandrusiaks@hotmail.com>
2015-07-12 17:21:54 -0700

    searches by room and date for create booking

--- RESTServer/routes/api/booking.js
+++ RESTServer/routes/api/booking.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Booking = require('../../models/booking');
 var User = require('../../models/user');
+var Room = require('../../models/room');
 
 module.exports.getAllBookings = function(req, res) {
     Booking.find(function(err, bookings) {
@@ -23,79 +24,79 @@ module.exports.createBooking = function(req, res, id) {
 
     var twoWeeks = new Date(year,month,(day+14));
 
-    //gets all bookings on specified date for specified room
-    //add in ,room: req.body.room as parameter once have room objects set up
-    Booking.find({date: req.body.date}, function(err, datesBookings) {
+    Room.find({roomID: req.body.roomId}, function(err, room){
         if(err) {
-          res.send(err);
+            res.send(err);
         }
 
-        //initializes booking
-        booking.date = req.body.date;
-        booking.numberOfPeople = req.body.numberOfPeople;
-        booking.canceledStatus = false;
-        booking.startTime = req.body.startTime;
-        booking.endTime = req.body.endTime;
-        
-        //check to ensure null objects are not passed in
-        //set to empty if object is null
-        if(req.body.room != null){
-            booking.room.push(req.body.room);
-        }
-        if(req.body.equipment != null){
-            booking.equipment.push(req.body.equipment);
-        }
-
-        
-        //checks to ensure booking is within two weeks of todays date and is not before todays date
-        if((booking.date > twoWeeks)){
-            return res.json({success: false, message: "Booking must be within two weeks of today"});
-        }
-        
-        if((booking.date <= noHoursToday)&&(booking.startTime < today.getHours())){
-            return res.json({success: false, message: "Cannot book on past dates" });
-        }
-
-        //error messages if booking times are between a different booking
-        for(var i=0; i<datesBookings.length; i++){
-            
-            //if new booking start time is between a previous booking start and end time return message
-            if((booking.startTime >= datesBookings[i].startTime) && (booking.startTime < datesBookings[i].endTime)){
-                return res.json({success: false, message: "Invalid. During current booking."});
-            }
-
-            //if new booking end time is between a previous booking start and end time return message
-            if((booking.endTime > datesBookings[i].startTime) && (booking.endTime <= datesBookings[i].endTime)){
-                return res.json({success: false, message: "Invalid. During current booking."});
-            }
-        }
-        //adds users username to booking
-        User.findById(id, function(err, user) {
+        //gets all bookings on specified date for specified room
+        Booking.find({date: req.body.date, room: room}, function(err, datesBookings) {
             if(err) {
-                res.send(err)
+            res.send(err);
             }
-            //if user banned do not allow create booking
-            if((user.banned) && (user.bannedUntil > today)) {
-                return res.json({success: false, message: "User is banned" });
-           
-            }else {
-                user.banned = false;
-                user.bookings.push(booking);
+
+            //initializes booking
+            booking.date = req.body.date;
+            booking.numberOfPeople = req.body.numberOfPeople;
+            booking.canceledStatus = false;
+            booking.startTime = req.body.startTime;
+            booking.endTime = req.body.endTime;
+            booking.room = room;
+        
+            if(req.body.equipment != null){
+                booking.equipment.push(req.body.equipment);
+            }
+
+        
+            //checks to ensure booking is within two weeks of todays date and is not before todays date
+            if((booking.date > twoWeeks)){
+                return res.json({success: false, message: "Booking must be within two weeks of today"});
+            }
+        
+            if((booking.date <= noHoursToday)&&(booking.startTime < today.getHours())){
+                return res.json({success: false, message: "Cannot book on past dates" });
+            }
+
+            //error messages if booking times are between a different booking
+            for(var i=0; i<datesBookings.length; i++){
             
-                user.save(function(err) {
+                //if new booking start time is between a previous booking start and end time return message
+                if((booking.startTime >= datesBookings[i].startTime) && (booking.startTime < datesBookings[i].endTime)){
+                    return res.json({success: false, message: "Invalid. During current booking."});
+                }
+
+                //if new booking end time is between a previous booking start and end time return message
+                if((booking.endTime > datesBookings[i].startTime) && (booking.endTime <= datesBookings[i].endTime)){
+                    return res.json({success: false, message: "Invalid. During current booking."});
+                }
+            }
+            //adds users username to booking
+            User.findById(id, function(err, user) {
+                if(err) {
+                    res.send(err)
+                }
+                //if user banned do not allow create booking
+                if((user.banned) && (user.bannedUntil > today)) {
+                    return res.json({success: false, message: "User is banned" });
+           
+                }else {
+                    user.banned = false;
+                    user.bookings.push(booking);
+            
+                    user.save(function(err) {
+                        if(err) return res.send(err);
+                    });
+           
+                    booking.username = user.username;
+                }
+
+
+                //save booking
+                booking.save(function(err) {
                     if(err) return res.send(err);
 
+                    res.json({success: true, booking: booking});
                 });
-           
-                booking.username = user.username;
-            }
-
-
-            //save booking
-            booking.save(function(err) {
-                if(err) return res.send(err);
-
-                res.json({success: true, booking: booking});
             });
         });
     });
